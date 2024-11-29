e56e92602cac4264c575b65f0db2fc90e4a0a818
cowhorse7 <becca11175@gmail.com>
2024-11-29 14:21:09 -0700

    smallProgress

--- server/src/main/java/server/ConnectionManager.java
+++ server/src/main/java/server/ConnectionManager.java
@@ -1,10 +1,35 @@
 package server;
-import model.UserData;
 import org.eclipse.jetty.websocket.api.Session;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Objects;
 
 public class ConnectionManager {
-    public HashMap<Integer, ArrayList<UserData>> gamesWithUsers = new HashMap<>();
+    public HashMap<Integer, ArrayList<String>> gamesToUsers = new HashMap<>();
+    public HashMap<String, Connection> allUsers = new HashMap<>();
+
+    public void add(String authToken, Session session, Integer gameID){
+        Connection currentUser = new Connection(authToken, session);
+        allUsers.put(authToken, currentUser);
+        ArrayList<String> players = new ArrayList<>();
+        players.add(authToken);
+        gamesToUsers.put(gameID, players);
+    }
+    public void remove(String authToken, Integer gameID){
+        //Integer index = findUser(authToken, gameID);
+        gamesToUsers.get(gameID).remove(authToken);
+    }
+//    private Integer findUser(String authToken, Integer gameID){
+//        ArrayList<String> users = gamesToUsers.get(gameID);
+//        String auth = "";
+//        int i = 0;
+//        for(i = 0; i < users.size(); i++) {
+//            auth = users.get(i);
+//            if(Objects.equals(auth, authToken)){
+//                break;
+//            }
+//        }
+//        return i;
+//    }
 }
--- server/src/main/java/server/WebsocketServer.java
+++ server/src/main/java/server/WebsocketServer.java
@@ -13,22 +13,27 @@ public class WebsocketServer {
 //            Spark.webSocket("/ws", WSServer.class);
 //            Spark.get("/echo/:msg", (req, res) -> "HTTP response: " + req.params(":msg"));
 //        }
+    ConnectionManager manager = new ConnectionManager();
 
         @OnWebSocketMessage
         public void onMessage(Session session, String message) throws Exception {
             session.getRemote().sendString("WebSocket response: " + message);
             UserGameCommand command = new Gson().fromJson(message, UserGameCommand.class);
             switch (command.getCommandType()){
-                case LEAVE -> leave();
-                case RESIGN -> resign();
-                case CONNECT -> connect();
+                case LEAVE -> leave(command.getAuthToken(), command.getGameID());
+                case RESIGN -> resign(command.getAuthToken(), command.getGameID());
+                case CONNECT -> connect(command.getAuthToken(), session, command.getGameID());
                 case MAKE_MOVE -> makeMove();
             }
         }
-        private void leave(){
-
+        private void leave(String authToken, Integer gameID){
+            manager.remove(authToken, gameID);
         }
-        private void resign(){}
-        private void connect(){}
+        private void resign(String authToken, Integer gameID){
+            leave(authToken,gameID);
+        }
+        private void connect(String authToken, Session session, Integer gameID){
+            manager.add(authToken, session, gameID);
+        }
         private void makeMove(){}
 }
