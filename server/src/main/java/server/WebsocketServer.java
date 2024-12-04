93c34119f7727dcbc5f513b0b452b0a2e14575c0
cowhorse7 <becca11175@gmail.com>
2024-12-03 22:14:04 -0700

    Passing nearly all tests

--- server/src/main/java/server/WebsocketServer.java
+++ server/src/main/java/server/WebsocketServer.java
@@ -26,7 +26,7 @@ public class WebsocketServer {
         this.authDataAccess = authDataAccess;
     }
     private String playerColor(GameData gameData){
-        String color = null;
+        String color = "";
         if (Objects.equals(gameData.whiteUsername(), user.username())){
             color = "white";
         }
@@ -86,7 +86,7 @@ public class WebsocketServer {
             GameData gameData = gameDataAccess.getGame(gameID);
             String color = playerColor(gameData);
             if (manager.gameEnded(gameID)){
-                message = "the game has already ended";
+                message = "the game has already ended\n";
                 ServerMessage toUser = new ErrorMessage(ServerMessage.ServerMessageType.ERROR, message);
                 manager.notifyUser(authToken, toUser);
             }
@@ -97,13 +97,19 @@ public class WebsocketServer {
                 manager.endGame(gameID);
             }
             else{
-                message = "observers cannot resign.";
+                message = "observers cannot resign.\n";
                 ServerMessage toUser = new ErrorMessage(ServerMessage.ServerMessageType.ERROR, message);
                 manager.notifyUser(authToken, toUser);
             }
         }
         private void connect(String authToken, Session session, Integer gameID) throws Exception {
             GameData gameData = gameDataAccess.getGame(gameID);
+            if(gameData == null) {
+                message = "game could not be found.\n";
+                ServerMessage toUser = new ErrorMessage(ServerMessage.ServerMessageType.ERROR, message);
+                manager.notifyUser(authToken, toUser);
+                return;
+            }
             String color = playerColor(gameData);
             ChessGame game = gameData.game();
             manager.add(authToken, session, gameID);
@@ -125,49 +131,67 @@ public class WebsocketServer {
             ChessGame game = gameData.game();
             ChessGame.TeamColor teamColor = game.getTeamTurn();
             if(manager.gameEnded(gameID)){
-                message = "you cannot play after the game has ended";
+                message = "you cannot play after the game has ended\n";
                 ServerMessage toUser = new ErrorMessage(ServerMessage.ServerMessageType.ERROR, message);
                 manager.notifyUser(authToken, toUser);
             }
             else if(!color.equals("white") && !color.equals("black")){
-                message = "observers cannot play";
+                message = "observers cannot play\n";
                 ServerMessage toUser = new ErrorMessage(ServerMessage.ServerMessageType.ERROR, message);
                 manager.notifyUser(authToken, toUser);
             }
             else if((color.equals("white") && !teamColor.equals(ChessGame.TeamColor.WHITE)) ||
                     (color.equals("black") && !teamColor.equals(ChessGame.TeamColor.BLACK))){
-                message = "you cannot move when it is not your turn";
+                message = "you cannot move when it is not your turn\n";
                 ServerMessage toUser = new ErrorMessage(ServerMessage.ServerMessageType.ERROR, message);
                 manager.notifyUser(authToken, toUser);
             }
             else {
                 ChessPosition start = move.getStartPosition();
                 ChessPosition end = move.getEndPosition();
+                try{
                 game.makeMove(move);
+                }catch (Exception ex){
+                    message = "invalid move\n";
+                    ServerMessage toUser = new ErrorMessage(ServerMessage.ServerMessageType.ERROR, message);
+                    manager.notifyUser(authToken, toUser);
+                    return;
+                }
+                gameData = new GameData(gameID, gameData.whiteUsername(), gameData.blackUsername(), gameData.gameName(), game);
+                gameDataAccess.updateGame(gameID, gameData);
+
                 ServerMessage onMove = new LoadGameMessage(ServerMessage.ServerMessageType.LOAD_GAME, game);
                 manager.notifyAllButUser(null, gameID, onMove);
+
                 message = String.format("%s moved piece at %d,%d to %d,%d.\n",
                         user.username(), start.getRow(), start.getColumn(), end.getRow(), end.getColumn());
                 ServerMessage toOthers = new NotificationMessage(ServerMessage.ServerMessageType.NOTIFICATION, message);
                 manager.notifyAllButUser(authToken, gameID, toOthers);
+
+                checkGameConditions(gameID, game, color);
+            }
+        }
+            private void checkGameConditions(Integer gameID, ChessGame game, String color) throws Exception {
+                ServerMessage toOthers;
                 if(color.equals("white")){color = "black";}
                 else if(color.equals("black")){color = "white";}
-                if (game.isInCheckmate(game.getTeamTurn())) {
-                    message = String.format("%s is in checkmate", color);
+                ChessGame.TeamColor teamColor = game.getTeamTurn();
+                if (game.isInCheckmate(teamColor)) {
+                    message = String.format("%s is in checkmate\n", color);
                     toOthers = new NotificationMessage(ServerMessage.ServerMessageType.NOTIFICATION, message);
                     manager.notifyAllButUser(null, gameID, toOthers);
                     manager.endGame(gameID);
-                } else if (game.isInCheck(game.getTeamTurn())) {
-                    message = String.format("%s is in check", color);
+                } else if (game.isInCheck(teamColor)) {
+                    message = String.format("%s is in check\n", color);
                     toOthers = new NotificationMessage(ServerMessage.ServerMessageType.NOTIFICATION, message);
                     manager.notifyAllButUser(null, gameID, toOthers);
-                } else if (game.isInStalemate(game.getTeamTurn())) {
-                    message = String.format("%s is in stalemate", gameData.gameName());
+                } else if (game.isInStalemate(teamColor)) {
+                    GameData gameData = gameDataAccess.getGame(gameID);
+                    message = String.format("%s is in stalemate\n", gameData.gameName());
                     toOthers = new NotificationMessage(ServerMessage.ServerMessageType.NOTIFICATION, message);
                     manager.notifyAllButUser(null, gameID, toOthers);
                     manager.endGame(gameID);
                 }
-            }
         }
 //        @OnWebSocketError
 //    public void errorTime(Throwable ex) throws Exception {
